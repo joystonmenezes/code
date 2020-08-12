2773815ca203306e81dac9d76d2667b4d9084856
joystonmenezes <53226233+joystonmenezes@users.noreply.github.com>
2020-08-12 16:26:28 +0530

    Create strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+
+void stringCpy(char* s1,char* s2);
+ 
+int main()
+{
+    char str1[100],str2[100];
+    
+    printf("Enter string 1: "); 
+    gets(str1);//read string with spaces
+    
+    stringCpy(str2,str1);
+    
+    printf("String 1: %s \nString 2: %s\n",str1,str2);
+    return 0;
+}
+ 
+/******** function definition *******/
+void stringCpy(char* s1,char* s2)
+{
+    int i=0;
+    while(s2[i]!='\0')
+    {
+        s1[i]=s2[i];
+        i++;
+    }
+    s1[i]='\0'; /*string terminates by NULL*/
+}
